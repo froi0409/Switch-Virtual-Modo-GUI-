42613a6d7008fd4c0c0d136a2476c97d3866da36
Fernando <ferrubocana@gmail.com>
2023-04-11 21:03:18 -0600

    Se corrigieron errores de CRC y respuesta de broadcast

--- SwitchApp/src/main/java/NetworkManagement/NetworkFrame.java
+++ SwitchApp/src/main/java/NetworkManagement/NetworkFrame.java
@@ -1,12 +1,14 @@
 package NetworkManagement;
 
 import java.io.Serializable;
+import java.util.zip.CRC32;
 
 
 public class NetworkFrame implements Serializable {
     private String message;
     private String macOrigin;
     private String macDestiny;
+    private String crc;
     /*
      * 1. Connect Device
      * 2. Own Client Message
@@ -22,6 +24,7 @@ public class NetworkFrame implements Serializable {
         this.macOrigin = macOrigin;
         if (type == 1) {
             this.message = "Agregando Dispositivo a la Tabla ARP";
+            this.crc = generateCrc(message);
         }
         /*else {
             this.message = "Buscando Dispositivos en la tabla ARP";
@@ -32,6 +35,7 @@ public class NetworkFrame implements Serializable {
         this.macOrigin = macOrigin;
         this.macDestiny = macDestiny;
         this.type = 3;
+        this.crc = generateCrc(message);
     }
 
     public NetworkFrame(int type, String message, String macOrigin, String macDestiny) {
@@ -39,6 +43,7 @@ public class NetworkFrame implements Serializable {
         this.macOrigin = macOrigin;
         this.macDestiny = macDestiny;
         this.type = type;
+        this.crc = generateCrc(message);
     }
 
     public String getMessage() {
@@ -47,6 +52,7 @@ public class NetworkFrame implements Serializable {
 
     public void setMessage(String message) {
         this.message = message;
+        this.crc = generateCrc(message);
     }
 
     public int getType() {
@@ -64,7 +70,18 @@ public class NetworkFrame implements Serializable {
     public String renderMessage() {
         String text = (type == 3) ? ("Mensaje Cliente\nOrigen: " + macOrigin + "\n") : "Mensaje Switch\n";
         text += "Destino: " + macDestiny + "\n";
+        text += "CRC: " + getCrc() + "\n";
         text += message + "\n\n";
         return text;
+    }
+
+    private String generateCrc(String data) {
+        CRC32 crc = new CRC32();
+        crc.update(data.getBytes());
+        return String.valueOf(crc.getValue());
+    }
+
+    public String getCrc() {
+        return crc;
     }
 }
